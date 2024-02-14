66d43fc2bba4dee346a62fbbee2e71675a1a9559
lde-mich <luca01112003@gmail.com>
2024-02-14 12:04:57 +0100

    upload-feb-14

--- ex00/Bureaucrat.cpp
+++ ex00/Bureaucrat.cpp
@@ -6,36 +6,33 @@
 /*   By: lde-mich <lde-mich@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/14 11:27:16 by lde-mich          #+#    #+#             */
-/*   Updated: 2024/02/14 11:29:21 by lde-mich         ###   ########.fr       */
+/*   Updated: 2024/02/14 11:35:32 by lde-mich         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
 
 
+Bureaucrat::Bureaucrat()
+{
 
+}
 
+Bureaucrat::Bureaucrat(Bureaucrat const &bureaucrat)
+{
+	(*this) = bureaucrat;
+}
 
+Bureaucrat::Bureaucrat(std::string name, int grade)
+{
+	this->name = name;
+	this->grade = grade;
+}
 
+Bureaucrat::~Bureaucrat()
+{
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}
 
 
 
--- ex00/Bureaucrat.hpp
+++ ex00/Bureaucrat.hpp
@@ -6,7 +6,7 @@
 /*   By: lde-mich <lde-mich@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/14 11:27:07 by lde-mich          #+#    #+#             */
-/*   Updated: 2024/02/14 11:29:26 by lde-mich         ###   ########.fr       */
+/*   Updated: 2024/02/14 11:42:15 by lde-mich         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,23 @@ class Bureaucrat
 {
 	private:
 
+		std::string name;
+		int grade;
 
 	public:
+
+		std::string const &getName() const;
+		int const &getGrade() const;
+
+		void increment();
+		void decrement();
+
+		Bureaucrat &operator=(Bureaucrat const &bureaucrat);
+
+		Bureaucrat(std::string name, int grade);
+		Bureaucrat(Bureaucrat const &bureaucrat);
+		Bureaucrat();
+		~Bureaucrat();
 };
 
 
