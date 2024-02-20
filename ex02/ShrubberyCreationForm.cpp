e40847416080fb6c65e361efe127d9b5aa2c5b7d
lde-mich <luca01112003@gmail.com>
2024-02-21 00:49:56 +0100

    upload-feb-21

--- ex02/ShrubberyCreationForm.cpp
+++ ex02/ShrubberyCreationForm.cpp
@@ -6,36 +6,36 @@
 /*   By: lde-mich <lde-mich@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/19 14:23:58 by lde-mich          #+#    #+#             */
-/*   Updated: 2024/02/21 00:29:39 by lde-mich         ###   ########.fr       */
+/*   Updated: 2024/02/21 00:47:26 by lde-mich         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ShrubberyCreationForm.hpp"
 
 std::string	tree =
-	"                     ___\n\
-                    _,-'\"\"   \"\"\"\"`--.\n\
-                 ,-'          __,,-- \\\n\
-               ,\'    __,--\"\"\"\"dF      )\n\
-              /   .-\"Hb_,--\"\"dF      /\n\
-            ,\'       _Hb ___dF\"-._,-'\n\
-          ,'      _,-\"\"\"\"   \"\"--..__\n\
-         (     ,-'                  `.\n\
-          `._,'     _   _             ;\n\
-           ,'     ,' `-'Hb-.___..._,-'\n\
-           \\    ,'\"Hb.-\'HH`-.dHF\"\n\
-            `--\'   \"Hb  HH  dF\"\n\
-                    \"Hb HH dF\n\
-                     \"HbHHdF\n\
-                      |HHHF\n\
-                      |HHH|\n\
-                      |HHH|\n\
-                      |HHH|\n\
-                      |HHH|\n\
-                      dHHHb\n\
-                    .dFd|bHb.               o\n\
+	"                             ___\n\
+                       _,-'\"\"   \"\"\"\"`--.\n\
+                    ,-'          __,,-- \\\n\
+                  ,\'    __,--\"\"\"\"dF      )\n\
+                 /   .-\"Hb_,--\"\"dF      /\n\
+               ,\'       _Hb ___dF\"-._,-'\n\
+             ,'      _,-\"\"\"\"   \"\"--..__\n\
+            (     ,-'                  `.\n\
+             `._,'     _   _             ;\n\
+              ,'     ,' `-'Hb-.___..._,-'\n\
+              \\    ,'\"Hb.-\'HH`-.dHF\"\n\
+               `--\'   \"Hb  HH  dF\"\n\
+                       \"Hb HH dF\n\
+                        \"HbHHdF\n\
+                         |HHHF\n\
+                         |HHH|\n\
+                         |HHH|\n\
+                         |HHH|\n\
+                         |HHH|\n\
+                         dHHHb\n\
+                       .dFd|bHb.               o\n\
 	      o       .dHFdH|HbTHb.          o /\n\
-	\\  Y  |  \\__,dHHFdHH|HHhoHHb.__Krogg  Y\n\
+	\\  Y  |  \\__,dHHFdHH|HHhoHHb.__       Y\n\
 	##########################################\n";
 
 
@@ -83,7 +83,8 @@ void ShrubberyCreationForm::execute(Bureaucrat const &executor) const
 
 	std::ofstream fout((this->target + "_shrubbery").c_str());
 
-	for(int i=0 ;i<200 ;i++)
+	int i = -1;
+	while(tree[++i])
 		fout<< tree[i];
 	fout.close();
 }
