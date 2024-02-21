8f70ea2d0933045a9f175d6466a859b6b869de62
lde-mich <luca01112003@gmail.com>
2024-02-21 14:02:56 +0100

    upload-feb-21

--- ex03/Intern.cpp
+++ ex03/Intern.cpp
@@ -6,7 +6,7 @@
 /*   By: lde-mich <lde-mich@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/21 11:00:32 by lde-mich          #+#    #+#             */
-/*   Updated: 2024/02/21 12:31:43 by lde-mich         ###   ########.fr       */
+/*   Updated: 2024/02/21 14:01:53 by lde-mich         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,15 +52,15 @@ AForm *Intern::makeForm(std::string nameForm, std::string target)
 	{
 		case 'R':
 			tmp = new RobotomyRequestForm(target);
-			std::cout << "Intern creates" << tmp->getName() << std::endl;
+			std::cout << "Intern creates " << tmp->getName() << std::endl;
 			return (tmp);
 		case 'S':
 			tmp = new ShrubberyCreationForm(target);
-			std::cout << "Intern creates" << tmp->getName() << std::endl;
+			std::cout << "Intern creates " << tmp->getName() << std::endl;
 			return (tmp);
 		case 'P':
 			tmp = new PresidentialPardonForm(target);
-			std::cout << "Intern creates" << tmp->getName() << std::endl;
+			std::cout << "Intern creates " << tmp->getName() << std::endl;
 			return (tmp);
 		default:
 			return (tmp);
--- ex03/main.cpp
+++ ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lde-mich <lde-mich@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/14 11:27:13 by lde-mich          #+#    #+#             */
-/*   Updated: 2024/02/19 12:24:45 by lde-mich         ###   ########.fr       */
+/*   Updated: 2024/02/21 14:01:03 by lde-mich         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,19 @@
 #include "ShrubberyCreationForm.hpp"
 #include "RobotomyRequestForm.hpp"
 #include "PresidentialPardonForm.hpp"
+#include "Intern.hpp"
 
 int main()
 {
 	try
 	{
-		Bureaucrat bureaucrat("Efesto", 2);
-		ShrubberyCreationForm form1("Asta");
-		RobotomyRequestForm form2("Minosse");
-		PresidentialPardonForm form3("President");
+		Bureaucrat bureaucrat("Franco", 2);
+		ShrubberyCreationForm form1("Shrubbery");
+		RobotomyRequestForm form2("Michele");
+		PresidentialPardonForm form3("Presidential");
+
+		Intern someRandomIntern;
+		AForm* rrf;
 
 		std::cout << "\n--------------- Form 1 ( Shrubbery ) ---------------" << std::endl;
 		bureaucrat.signForm(form1);
@@ -40,9 +44,12 @@ int main()
 		bureaucrat.executeForm(form2);
 		bureaucrat.executeForm(form2);
 
-		std::cout << "\n--------------- Form 3 ( President ) ---------------" << std::endl;
+		std::cout << "\n--------------- Form 3 ( Presidential ) ---------------" << std::endl;
 		bureaucrat.signForm(form3);
 		bureaucrat.executeForm(form3);
+
+		rrf = someRandomIntern.makeForm("Error request", "Bender");
+		rrf->getName();
 	}
 	catch (std::exception &e)
 	{
